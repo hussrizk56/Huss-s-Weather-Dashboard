bf047070b847cdbe0df15ae1da79c9501f16bff7
hussrizk56 <117960857+hussrizk56@users.noreply.github.com>
2022-12-31 14:56:40 -0500

    updated html with the body code and linked element for todays weather and the 5 day forcast

--- Assets/script.js
+++ Assets/script.js
@@ -1,3 +1,9 @@
 
 
-var APIKEY = bd625c75b2eb876533b63e53efd22b4f
+
+
+        const apiKey = `bd625c75b2eb876533b63e53efd22b4f`;
+        // Constructs a query URL (template literal). 
+        const queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${caseFix}&appid=${apiKey}&units=imperial`;
+       
+    
--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # Huss-s-Weather-Dashboard
 
 
-
